cc588b895402412e8986f86c5a1b233761388c3f
Zabarina <svetoch.z.2011@gmail.com>
2025-11-01 14:14:10 +0300

    feat: add Redux, store with UserSlice

--- src/features/user/UserName.tsx
+++ src/features/user/UserName.tsx
@@ -1,5 +1,14 @@
+import {useSelector} from "react-redux";
+import type {RootState} from "../../store.ts";
+
 const UserName = () => {
-  return <div className="text-sm font-semibold hidden md:block">Sveta</div>
+  const username: string = useSelector((state: RootState) => state.user.username)
+
+  if (!username) return null;
+  return (
+    <div className="text-sm font-semibold hidden md:block">
+      {username}
+    </div>)
 }
 
 export default UserName
